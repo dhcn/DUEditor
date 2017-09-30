a9f20189af09f0c0f257fe3fec01897ce02d271d
dengel <dhcn@163.com>
2017-09-30 11:27:26 +0800

    include tag & others

---
+++ DUEditor/templatetags/ueditor_tags.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+'''
+Created on 2013-7-4
+
+@author: hulda
+'''
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag('ueditorjs.html', takes_context=True)
+def ueditor(context,editor_id):
+
+    return {"editor_id":editor_id}
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@
 - 存储基本都使用Django的Storage,便于快速切换不同的文件存储backends
 - UEditor版本升级到最新的1.4.33
 - 做了一定的安全加固和配置简化
-- 准备做一些易用的include tag
+- 开发易用的include tag
 - 因为配置个性化问题，暂时不准备支持pip install，使用者请直接把DUEditor拉到自己项目目录即可。
 
 2017-9-27:这个分支更名为:DUEditor
